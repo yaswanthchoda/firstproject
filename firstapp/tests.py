09384dc86b8ecef5444a2337634981a7b441f460
itrois053 <yaswanthkumar_choda@yahoo.in>
2018-09-29 11:16:00 +0530

    test case

--- firstapp/tests.py
+++ firstapp/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
+import json
 
 # Create your tests here.
+class TestAdd(TestCase):
+	
+	def setup(self):
+		self.client = Client()
+
+	def test_add(self):
+		c = self.client.post(reverse('add'), data={'name':"yyyy", "address":"jjjjj"})
+		c = json.loads(c.content)
+		self.assertEqual(c["status"],"success")
