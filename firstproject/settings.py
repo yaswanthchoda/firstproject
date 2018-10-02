ef12071e4796ddf2122dc3d270e4870465bdd8ff
yaswanthchoda <yaswanthkumar_choda@yahoo.in>
2018-10-02 09:24:44 +0530

    removed commented tasks from jenkin tasks

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -77,10 +77,6 @@ WSGI_APPLICATION = 'firstproject.wsgi.application'
 
 JENKINS_TASKS = (
     'django_jenkins.tasks.run_pep8',
-    # 'django_jenkins.tasks.run_pyflakes',
-    # 'django_jenkins.tasks.run_jslint',
-    # 'django_jenkins.tasks.run_csslint',    
-    # 'django_jenkins.tasks.run_sloccount'
 )
 
 
